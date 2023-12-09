9ee8da10287e72e316099d86706345ca06837fd5
theeraphop44 <149029833+theeraphop44@users.noreply.github.com>
2023-12-09 22:48:02 +0700

    update

--- src/script.js
+++ src/script.js
@@ -1,66 +1,62 @@
-// Map Initialization
-var map = L.map('map').setView([14.05000, 100.48333], 10);
+var map = L.map('map').setView([13.80838, 100.49487], 10);
 
 // Tile Layer
 var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var geojson; // Declare geojson variable
 
-// Update Map
-var info = L.control();
-info.onAdd = function (map) {
-    this._div = L.DomUtil.create('div', 'info');
-    this.update();
-    return this._div;
-};
-
-info.update = function (props) {
-    var contents = props ? `<b>${props.name}</b><br />${props.value} people / mi<sup>2</sup>` : 'Hover over a state';
-    this._div.innerHTML = `<h4>Phathum Population Value</h4>${contents}`;
-};
-
-info.addTo(map);
-
-// GeoJSON Data Fetch
-fetch("Map_Phathum.geojson")
-    .then(response => response.json())
-    .then(data => {
-        // Create a GeoJSON layer
-        geojson = L.geoJSON(data, {
-            style: function (feature) {
-                var value = feature.properties.value;
-                return {
-                    fillColor: getColor(value),
-                    weight: 2,
-                    opacity: 1,
-                    color: 'blue',
-                    dashArray: '3',
-                    fillOpacity: 0.7
-                };
-            },
-            onEachFeature: function (feature, layer) {
-                layer.bindPopup('Region: ' + feature.properties.name + '<br>Value: ' + feature.properties.value);
-                layer.on({
-                    mouseover: highlightFeature,
-                    mouseout: resetHighlight,
-                    click: zoomToFeature
-                });
-            },
-        }).addTo(map);
-        console.log(data);
-    })
-    .catch(error => console.error("Error loading GeoJSON:", error));
+let geojson;
 
 // Color function
 function getColor(e) {
-    return e > 20 ? 'red' :
-        e > 10 ? 'rgb(255, 157, 0)' :
-            e > 5 ? 'rgb(255, 242, 0)' :
-                e > 2 ? 'rgb(4, 255, 0)' :
-                    'rgb(4, 255, 0)';
+    return e > 100 ? 'red' :
+        e > 70 ? 'blue' :
+            e > 30 ? 'rgb(8, 206, 28)' :
+                'rgb(252, 236, 55)';
 }
+
+async function someName() {
+
+    defaut_show("ธัญบุรี", "ฟิวเจอร์ฯรังสิต")
+    const waterLevel = await fetch("https://promethazine.holfz.workers.dev/waterlevel", { method: 'GET', redirect: 'follow' });
+    const waterLevelResp = await waterLevel.json();
+
+    const response = await fetch("Map_Phathum.geojson");
+    const data = await response.json();
+
+    geojson = L.geoJSON(data, {
+        style: function (feature) {
+            const thisStation = waterLevelResp.data.find(o => (o.geocode.amphoe_name === feature.properties.name));
+            if (!thisStation) {
+                // console.log(feature);
+            }
+            //    console.log(thisStation);
+            return {
+                fillColor: getColor(thisStation.storage.percent), // นำค่า value ไปใช้กับ getColor() เพื่อกำหนดสี
+                weight: 2,
+                opacity: 1,
+                color: 'black',
+                dashArray: '3',
+                fillOpacity: 0.7
+            };
+
+        },
+
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup('Region: ' + feature.properties.name);
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+                click: zoomToFeature
+            })
+        }
+
+    }).addTo(map);
+
+}
+
+someName()
 
 // Style function
 function style(feature) {
@@ -87,57 +83,189 @@ function highlightFeature(e) {
     layer.bringToFront();
 }
 
+async function defaut_show(loca, station) {
+    const rain = await fetch("https://promethazine.holfz.workers.dev/rainlevel", { method: 'GET', redirect: 'follow' });
+    const rainResp = await rain.json();
+    const waterLevel = await fetch("https://promethazine.holfz.workers.dev/waterlevel", { method: 'GET', redirect: 'follow' });
+    const waterLevelResp = await waterLevel.json();
+    const thisStation = waterLevelResp.data.find(o => (o.geocode.amphoe_name === loca));
+
+    console.log(thisStation);
+
+    const name = thisStation.geocode.amphoe_name;
+    const water_level = thisStation.waterlevel_msl;
+    const msl = thisStation.waterlevel_bank.msl;
+    const statusofwater = thisStation.storage.situation_text;
+    const update_water = thisStation.update_at;
+    const storagepercent = thisStation.storage.percent;
+
+
+    const statuslevelwater_name = document.getElementById('statuslevelwater-name');
+    statuslevelwater_name.textContent = ` ${name}`;
+
+    const statuslevelwater_waterlevel = document.getElementById('statuslevelwater-waterlevel');
+    statuslevelwater_waterlevel.textContent = `ระดับน้ำ : ${water_level}`;
+
+    const statuslevelwater_bank = document.getElementById('statuslevelwater-bank');
+    statuslevelwater_bank.textContent = `ระดับตลิ่ง : ${msl}`;
+
+    const statuslevelwater_status = document.getElementById('statuslevelwater-status');
+    statuslevelwater_status.textContent = `${statusofwater}`;
+
+    if (storagepercent > 100) {
+        statuslevelwater_status.style.backgroundColor = "red";
+    } else if (storagepercent > 70) {
+        statuslevelwater_status.style.backgroundColor = "blue";
+    } else if (storagepercent > 30) {
+        statuslevelwater_status.style.backgroundColor = "rgb(8, 206, 28)";
+    } else {
+        statuslevelwater_status.style.backgroundColor = "rgb(252, 236, 55)";
+    }
+    statuslevelwater_status.style.color = "white";
+
+    const statuslevelwater_date = document.getElementById('statuslevelwater-date');
+    statuslevelwater_date.textContent = `วันที่ : ${update_water}`;
+
+
+    const thisrainStation = rainResp.data.find(o => (o.station.name === station));
+
+    const tableData = thisrainStation.rain7d.map((data, index) => ({
+        num: index + 1,
+        date: data.date,
+        situation: data.rain.situation_text,
+    }));
+
+    const currentRainSituation = thisrainStation.current_rain.situation_text;
+    const rain_collection = thisrainStation.rain_collection;
+
+    // แสดงข้อมูลจาก rain_collection ใน HTML
+    const rainCollection1dElement = document.getElementById('rainCollection1d');
+    const rainCollection3dElement = document.getElementById('rainCollection3d');
+    const rainCollection7dElement = document.getElementById('rainCollection7d');
+
+    // แสดงข้อมูล rain_collection ใน HTML
+    rainCollection1dElement.textContent = `${rain_collection["1d"]}`;
+    rainCollection3dElement.textContent = `${rain_collection["3d"]}`;
+    rainCollection7dElement.textContent = `${rain_collection["7d"]}`;
+
+    // แสดง icon
+    const currentRainIconElement = document.getElementById('currentRainIcon');
+    const currentRainTextElement = document.getElementById('currentRainText');
+
+
+    switch (currentRainSituation) {
+        case 'ไม่มีฝน':
+            currentRainIconElement.textContent = '☀️';
+            break;
+        case 'มีฝน':
+            currentRainIconElement.textContent = '🌧️';
+            break;
+        case 'ฝนตกเล็กน้อย':
+            currentRainIconElement.textContent = '🌦️';
+            break;
+        default:
+            currentRainIconElement.textContent = '❓';
+    }
+
+    // แสดงข้อความ
+    currentRainTextElement.textContent = currentRainSituation;
+
+    // ทำการ sort ข้อมูล tableData ตาม date
+    tableData.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    // Add new rows to the table
+    tableBody.innerHTML = '';
+
+    // Add new rows to the table
+    tableData.forEach(item => {
+        const row = document.createElement('tr');
+        row.classList.add(item.num % 2 === 0 ? 'TableRow-b' : 'TableRow-w');
+
+        // Create cell for date
+        const dateCell = document.createElement('td');
+        dateCell.classList.add('TableCell-body');
+        dateCell.textContent = item.date;
+        row.appendChild(dateCell);
+
+        // Create cell for situation icon
+        const situationCell = document.createElement('td');
+        situationCell.classList.add('TableCell-body');
+
+        // Create icon span
+        const icon = document.createElement('span');
+
+        switch (item.situation) {
+            case 'ไม่มีฝน':
+                icon.textContent = '☀️'; // ตัวอย่าง icon สำหรับไม่มีฝน (แดด)
+                break;
+            case 'มีฝน':
+                icon.textContent = '🌧️'; // ตัวอย่าง icon สำหรับมีฝน
+                break;
+            case 'ฝนตกเล็กน้อย':
+                icon.textContent = '🌦️'; // ตัวอย่าง icon สำหรับฝนตกเล็กน้อย
+                break;
+            // เพิ่ม case ตามสภาพอากาศอื่น ๆ ตามต้องการ
+            default:
+                icon.textContent = '❓'; // ตัวอย่าง icon สำหรับสภาพอากาศที่ไม่ทราบ
+        }
+
+        situationCell.appendChild(icon);
+        row.appendChild(situationCell);
+
+        // Create cell for situation text
+        const situationTextCell = document.createElement('td');
+        situationTextCell.classList.add('TableCell-body');
+        situationTextCell.textContent = item.situation;
+        row.appendChild(situationTextCell);
+
+        tableBody.appendChild(row);
+    });
+}
+
 function resetHighlight(e) {
     geojson.resetStyle(e.target);
 }
 
 let isZoomed = false; // เพิ่มตัวแปร isZoomed
 
-function zoomToFeature(e) {
-    var layer = e.target;
-    var properties = layer.feature.properties;
+async function zoomToFeature(e) {
+    if (e) {
+        var layer = e.target;
+        var properties = layer.feature.properties;
 
-    if (isZoomed && map.getBounds().equals(layer.getBounds())) {
-        isZoomed = false;
-        return;
+        if (properties.name == "ธัญบุรี") {
+            xx = "ฟิวเจอร์ฯรังสิต"
+        } else if (properties.name == "หนองเสือ") {
+            xx = "คลองระพีพัฒน์แยกใต้ หนองเสือ"
+        } else if (properties.name == "เมืองปทุมธานี") {
+            xx = "คลองเปรมประชากร หลักหก"
+        } else if (properties.name == "คลองหลวง") {
+            xx = "คลองระพีพัฒน์แยกตก"
+        } else if (properties.name == "ลำลูกกา") {
+            xx = "คลองหกวา ลำลูกกา คลอง8"
+        }
+
+        if (isZoomed && map.getBounds().equals(layer.getBounds())) {
+            isZoomed = false;
+            return;
+        }
+
+        // ตรวจสอบว่าคลิกที่ feature อื่นหรือไม่
+        if (isZoomed) {
+            isZoomed = false;
+        }
+
     }
+    defaut_show(properties.name, xx);
+    // If e is not null, fit bounds to the layer
+    if (e) {
+        map.fitBounds(layer.getBounds());
 
-    // ตรวจสอบว่าคลิกที่ feature อื่นหรือไม่
-    if (isZoomed) {
-        isZoomed = false;
+        // ทำเครื่องหมายว่ามีการ zoom แล้ว
+        isZoomed = true;
     }
-
-    // Show data in the table
-    const tableData = [
-        { num: 1, location: properties.name, waterLevel: "1", status: properties.value, time: "3" },
-        // Add more rows as needed
-    ];
-
-    // Clear existing table content
-    tableBody.innerHTML = '';
-    
-    // Add new rows to the table
-    tableData.forEach(item => {
-        const row = document.createElement('tr');
-        row.classList.add(item.num % 2 === 0 ? 'TableRow-b' : 'TableRow-w');
-
-        Object.entries(item).forEach(([key, value]) => {
-            if (key !== 'num') {
-                const cell = document.createElement('td');
-                cell.classList.add('TableCell-body');
-                cell.textContent = value;
-                row.appendChild(cell);
-            }
-        });
-
-        tableBody.appendChild(row);
-    });
-
-    map.fitBounds(layer.getBounds());
-
-    // ทำเครื่องหมายว่ามีการ zoom แล้ว
-    isZoomed = true;
 }
+
 
 map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
 
@@ -145,7 +273,7 @@ const legend = L.control({ position: 'bottomright' });
 
 legend.onAdd = function (map) {
     const div = L.DomUtil.create('div', 'info legend');
-    const grades = [2, 5, 10, 20];
+    const grades = [0, 30, 70, 100];
     const labels = [];
     let from, to;
 
@@ -164,8 +292,7 @@ legend.addTo(map);
 
 
 const data = [
-    { num: 1, location: "", waterLevel: "", status: "", time: "" },
-    
+    { num: 1, Day: "", สภาพอากาศ: "", สถานะ: "" },
 ];
 
 const tableBody = document.querySelector('.TableBody');
@@ -202,4 +329,16 @@ data.forEach(item => {
 
     // เพิ่ม <tr> ลงใน tbody
     tableBody.appendChild(row);
+});
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+
+        this.style.background = '#ff0000';
+
+        // อย่าลืมดำเนินการอื่นๆ ตามที่คุณต้องการ
+    });
 });
